9e07c7253f137ef908ece9cee8f22111a353b6f7
pieterjoubert <pieter.joubert@gmail.com>
2020-02-19 10:12:47 +0200

    Initial commit

---
+++ GADE62111_2020_CharSheet/GADE62111_2020_CharSheet/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GADE62111_2020_CharSheet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
